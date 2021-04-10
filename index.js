4f57bf75a30470e08b90d0babc6719230ed694b1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-04-10 12:15:59 +0200

    :sparkles: Scrape fully vaccinated percent from covid-vaccinatie.be

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const fetch = require('node-fetch');
+
+const getFullyVaccinatedPercent = async () => {
+  try {
+    const response = await fetch('https://covid-vaccinatie.be/en');
+    const pageHtml = await response.text();
+    const html = pageHtml.match(/(?<=Fully vaccinated).*?(?=of the population)/is)[0];
+    const percent = html.match(/([\d.]+?)%/)[0];
+
+    return parseFloat(percent) / 100.0;;
+  } catch {
+    console.log('Could not scrape percentage');
+    return null;
+  }
+};
+
+(async () => {
+  console.log(await getFullyVaccinatedPercent());
+})();
